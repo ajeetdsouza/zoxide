d44e66e907975d4964a9aebee53a7bb1dbbe7a7c
Klaus Alexander Seiﬆrup <klaus@seistrup.dk>
2020-03-10 13:25:14 +0100

    Make z more plugin compatible with cd

--- functions/z.fish
+++ functions/z.fish
@@ -1,13 +1,18 @@
 function z
-    if test (count $argv) -gt 0
-        set _Z_RESULT (zoxide query $argv)
-        switch "$_Z_RESULT"
-            case 'query: *'
-                cd (string sub -s 8 -- "$_Z_RESULT")
-                commandline -f repaint
-            case '*'
-                echo -n "$_Z_RESULT"
-        end
+  if test (count $argv) -gt 0
+    if test "$argv[1]" = "-"
+      cd -
+    else
+      set _Z_RESULT (zoxide query $argv)
+      switch "$_Z_RESULT"
+        case 'query: *'
+          cd (string sub -s 8 -- "$_Z_RESULT")
+          commandline -f repaint
+        case '*'
+          echo -n "$_Z_RESULT"
+      end
     end
+  else
+    cd ~
+  end
 end
-

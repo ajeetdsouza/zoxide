3f743e63a009b2639e9ed59c43f7d7eb5585d8b7
Ajeet D'Souza <98ajeet@gmail.com>
2021-04-03 15:12:08 +0530

    fix

--- shell.nix
+++ shell.nix
@@ -3,27 +3,25 @@ let
   pkgsMaster = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/470e4a9bbc98b171a7e733dfc9e62531f7b9ceca.tar.gz") {};
   pkgsPython = pkgs.python3.withPackages (pkgs: [ pkgs.black pkgs.mypy pkgs.pylint ]);
 in
-pkgs.mkShell
-  {
-    name = "env";
-    buildInputs = [
-      pkgsMaster.nushell
-      pkgsPython
-      pkgs.bash
-      pkgs.cargo
-      pkgs.cargo-audit
-      pkgs.dash
-      pkgs.fish
-      pkgs.fzf
-      pkgs.git
-      pkgs.powershell
-      pkgs.rustc
-      pkgs.rustfmt
-      pkgs.shellcheck
-      pkgs.shfmt
-      pkgs.xonsh
-      pkgs.zsh
-    ];
-
-    RUST_BACKTRACE = 1;
-  }
+pkgs.mkShell {
+  name = "env";
+  buildInputs = [
+    pkgsMaster.nushell
+    pkgsPython
+    pkgs.bash
+    pkgs.cargo
+    pkgs.cargo-audit
+    pkgs.dash
+    pkgs.fish
+    pkgs.fzf
+    pkgs.git
+    pkgs.powershell
+    pkgs.rustc
+    pkgs.rustfmt
+    pkgs.shellcheck
+    pkgs.shfmt
+    pkgs.xonsh
+    pkgs.zsh
+  ];
+  RUST_BACKTRACE = 1;
+}

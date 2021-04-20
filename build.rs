30911a1580aaaa5769fc5289ae84d01bfb45a21d
Ajeet D'Souza <98ajeet@gmail.com>
2021-04-20 11:50:54 +0530

    Use git2 to remove Git dependency

--- build.rs
+++ build.rs
@@ -1,16 +1,24 @@
-use std::process::Command;
+use git2::{DescribeFormatOptions, DescribeOptions, Repository};
+
+use std::env;
+
+fn git_version() -> Option<String> {
+    let path = env::var("CARGO_MANIFEST_DIR").ok()?;
+    let repository = Repository::open(path).ok()?;
+
+    let mut options = DescribeOptions::new();
+    options.describe_tags();
+    let describe = repository.describe(&options).ok()?;
+
+    let mut format = DescribeFormatOptions::new();
+    format.dirty_suffix("-dirty");
+
+    describe.format(Some(&format)).ok()
+}
 
 fn main() {
-    let git_describe = Command::new("git")
-        .args(&["describe", "--tags", "--broken"])
-        .output()
-        .ok()
-        .and_then(|proc| String::from_utf8(proc.stdout).ok());
-
-    let version_info = match git_describe {
-        Some(description) if !description.is_empty() => description,
-        _ => format!("v{}", env!("CARGO_PKG_VERSION")),
-    };
-
-    println!("cargo:rustc-env=ZOXIDE_VERSION={}", version_info);
+    let version = git_version()
+        .or_else(|| env::var("CARGO_PKG_VERSION").ok())
+        .unwrap_or_else(|| "unknown".to_string());
+    println!("cargo:rustc-env=ZOXIDE_VERSION={}", version);
 }

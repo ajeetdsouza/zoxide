9d8bfc90be1f990619501271f8209e2d8a4f79bb
EdJoPaTo <github@edjopato.de>
2021-10-29 20:31:25 +0200

    Improve release action (#284)

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let
   rust = import (builtins.fetchTarball
     "https://github.com/oxalica/rust-overlay/archive/ad311f5bb5c5ef475985f1e0f264e831470a8510.tar.gz");
   pkgs = import <nixpkgs> { overlays = [ rust ]; };
-  pkgs-latest = import (fetchTarball
+  pkgs-latest = import (builtins.fetchTarball
     "https://github.com/NixOS/nixpkgs/archive/3ef1d2a9602c18f8742e1fb63d5ae9867092e3d6.tar.gz")
     { };
 in pkgs.mkShell {

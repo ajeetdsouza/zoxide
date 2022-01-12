e34b6f930a804a88889536ec87d808ce20bc061f
cherryblossom000 <31467609+cherryblossom000@users.noreply.github.com>
2022-01-12 18:03:58 +1100

    Use elvish v0.17 lambda syntax in completions generation (#334)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,6 +16,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Fixed
 
 - Rename `_z` completion function to avoid conflicts with other shell plugins.
+- Elvish: upgrade to new lambda syntax.
 
 ## [0.8.0] - 2021-12-25
 
--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let
   rust = import (builtins.fetchTarball
     "https://github.com/oxalica/rust-overlay/archive/203dc4fc3fe2a5df1aa481a3fc8a1bb27074d677.tar.gz");
   pkgs = import (builtins.fetchTarball
-    "https://github.com/NixOS/nixpkgs/archive/eac07edbd20ed4908b98790ba299250b5527ecdf.tar.gz") {
+    "https://github.com/NixOS/nixpkgs/archive/fae46e66a5df220327b45e0d7c27c6961cf922ce.tar.gz") {
       overlays = [ rust ];
     };
 in pkgs.mkShell {

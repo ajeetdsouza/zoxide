f19351fefe609ad57b22fcfe4ed46f2bfd590423
Ajeet D'Souza <98ajeet@gmail.com>
2022-03-31 03:39:39 +0530

    Update nixpkgs

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let
   rust = import (builtins.fetchTarball
     "https://github.com/oxalica/rust-overlay/archive/46d8d20fce510c6a25fa66f36e31f207f6ea49e4.tar.gz");
   pkgs = import (builtins.fetchTarball
-    "https://github.com/NixOS/nixpkgs/archive/fae46e66a5df220327b45e0d7c27c6961cf922ce.tar.gz") {
+    "https://github.com/NixOS/nixpkgs/archive/d19a9162c848517cfc9437f10945b736d718b948.tar.gz") {
       overlays = [ rust ];
     };
 in pkgs.mkShell {

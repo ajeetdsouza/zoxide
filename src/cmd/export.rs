1f4a75f374c4bb485d9bd624fabafacfa8aa09d9
Jnyandeep <thokchom.jnyandeep@gmail.com>
2026-07-30 11:14:09 +0530

    fix: linting error

--- src/cmd/export.rs
+++ src/cmd/export.rs
@@ -2,11 +2,10 @@ use std::io::{self, Write};
 
 use serde_json::to_writer_pretty;
 
-use crate::{
-    cmd::{Export, ExportCommand, Run},
-    db::Database,
-    error::BrokenPipeHandler,
-};
+use crate::cmd::{Export, ExportCommand, Run};
+use crate::db::Database;
+use crate::error::BrokenPipeHandler;
+
 
 impl Run for Export {
     fn run(&self) -> anyhow::Result<()> {
